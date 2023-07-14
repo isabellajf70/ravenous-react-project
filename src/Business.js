d2afcb9c41014368669d11149096cfc2e05a3075
IsabellaFu <jf3286@cumc.columbia.edu>
2023-07-14 23:55:51 +0800

    added business

---
+++ src/Business.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const business = {
+  imageSrc:
+    "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
+  name: ,
+  address: ,
+  city: ,
+  state: ,
+  zipCode: ,
+  category: ,
+  rating: ,
+  reviewCount: ,
+};
+
+export default business;
+
+
+
+
+
+
+
+
+
